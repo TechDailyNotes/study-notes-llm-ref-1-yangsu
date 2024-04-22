108c001025a235640db2a996fe3b2527a1821609
Yang Su <ys724@cornell.edu>
2024-04-22 16:52:49 -0400

    Update basic_equations.py

--- basic_equations.py
+++ basic_equations.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import torch.nn.functional as F
 
 ### ML
 
@@ -18,44 +19,79 @@ def attention(x, w_q, w_k, w_v, mask):
 	qk_t = q @ k_t # qk_t shape (seq_len x seq_len)
 	qk_t /= sqrt(dim_q/k) # scale down by dim_q/k, yes
 	qk_t.masked_fill(mask == 0, float("-inf")) # mask -inf to softmax to 0
-	attn = softmax(qk_t, dim=-1) # along last seq_len dim
+	attn = F.softmax(qk_t, dim=-1) # along last seq_len dim
 	output = qk_t @ v # output shape (seq_len x dim_v)
 	return output
 
 # attention for multi-batches
-# memory same O(.)
+# memory O(3 * (embed_dim ** 2 + embed_dim))
 # X_batch: shape (batch_size x seq_len x embed_dim)
-# w_q, w_k, w_v: same as above
+# w_q, w_k, w_v: shape (embed_dim, embed_dim)
 # mask_batch: shape (batch_size x seq_len x seq_len)
 def batch_attention(X_batch, w_q, w_k, w_v, mask_batch):
-	Q = X_batch @ w_q # Q shape (batch_size x seq_len x dim_q/k)
-	K = X_batch @ w_k # K shape (batch_size x seq_len x dim_q/k)
-	V = X_batch @ w_v # V shape (batch_size x seq_len x dim_v)
-	K_t = K.T # K_t shape (batch_size x dim_q/k x seq_len)
+	Q = X_batch @ w_q # Q shape (batch_size x seq_len x embed_dim)
+	K = X_batch @ w_k # K shape (batch_size x seq_len x embed_dim)
+	V = X_batch @ w_v # V shape (batch_size x seq_len x embed_dim)
+	K_t = K.T # K_t shape (batch_size x embed_dim x seq_len)
 	K_t = K.permute(0, 2, 1) # equivalent, batch_size dim does NOT transpose
-	QK_t = torch.bmm(Q, K_t) # QK_t shape (batch_size x seq_len x seq_len)
-	QK_t /= sqrt(dim_q/k)
+	QK_t = torch.matmul(Q, K_t) # QK_t shape (batch_size x seq_len x seq_len)
+	QK_t /= sqrt(embed_dim)
 	# QK_t += position_bias, shape (1 x seq_len x seq_len)
-	QK_t.masked_fill(mask_batch == 0, float("-inf"))
-	attn = softmax(QK_t, dim=-1)
-	output = torch.bmm(QK_t, V) # output shape (batch_size x seq_len x dim_v)
+	QK_t = QK_t.masked_fill(mask_batch == 0, float("-inf"))
+	attn = F.softmax(QK_t, dim=-1)
+	output = torch.matmul(QK_t, V) # output shape (batch_size x seq_len x embed_dim)
 	return output
 
-# multihead attention for multi-batches, let hidden_dim = embed_dim x num_head
+# MHA (Multihead attention)
+# let hidden_dim = embed_dim x num_head
 # memory O(4 * (hidden_dim ** 2 + hidden_dim))
-# X_batch: same as above
-# W_q, W_k, W_v: embed dim multiplied by head (embed_dim x num_head x dim_q/k/v)
+# time O(seq_len ** 2 * hidden_dim)
+# X_batch: shape (batch_size x seq_len x hidden_dim)
+# W_qkv: embed dim multiplied by head (hidden_dim x (3 * hidden_dim))
 # W_o: linear layer after multihead output are computed (hidden_dim x hidden_dim)
-# attn has an additional shape of num_head (batch_size x seq_len x seq_len x num_head)
+# attn has an additional shape of num_head (batch_size x num_head x seq_len x seq_len)
+# mask_batch: shape (batch_size x seq_len x seq_len)
 # same mask_batch is applied to all heads
 # output is concat by num_head, shape (batch_size x seq_len x hidden_dim)
 # final output is computed going through W_o
-def multihead_batch_attention(X_batch, W_q, W_k, W_v, W_o, mask_batch):
-	# Q shape (batch_size x seq_len x num_head x dim_q)
-	# K_t shape (batch_size x seq_len x dim_k x num_head)
+def multi_head_attention(X_batch, W_qkv, W_o, mask_batch):
+	QKV = X_batch @ W_qkv # QKV shape (batch_size x seq_len x (3 * hidden_dim))
+	QKV = QKV.reshape(batch_size, seq_len, num_head, 3 * embed_dim)
+	QKV = QKV.permute(0, 2, 1, 3) # QKV shape (batch_size x num_head x seq_len x (3 * embed_dim))
+	Q, K, V = QKV.split(3, dim=-1)
+	K_t = K.permute(-2, -1) # K_t shape (batch_size x num_head x embed_dim x seq_len)
 	QK_t = torch.matmul(Q, K_t) # shape (batch_size x num_head x seq_len x seq_len)
-	# same until output
-	return torch.bmm(output, W_o)
+	QK_t /= sqrt(embed_dim) # not hidden_dim! remember the distribution of this
+	# QK_t += position_bias, shape (1 x seq_len x seq_len)
+	mask_batch = mask_batch.unsqueeze(1) # (batch_size x 1 x seq_len x seq_len)
+	QK_t = QK_t.masked_fill(mask_batch == 0, float("-inf"))
+	attn = F.softmax(QK_t, dim=-1)
+	values = torch.matmul(attn, V) # shape (batch_size x num_head x seq_len x embed_dim)
+	values = values.permute(0, 2, 1, 3) # shape (batch_size x seq_len x num_head x embed_dim)
+	values = values.reshape(batch_size, seq_len, hidden_dim) # shape (batch_size x seq_len x hidden_dim)
+	output = values @ W_o # shape (batch_size x seq_len x hidden_dim)
+	return output
+
+# MQA (Multihead Query Attention)
+# let hidden_dim = num_head x embed_dim
+# W_q: multi-head, shape (batch_size x seq_len x hidden_dim)
+# W_kv: single-head, shape (batch_size x seq_len x (2 * embed_dim))
+def multi_query_attention(X_batch, W_q, W_kv, W_o, mask_batch):
+	pass
+
+# GQA (Grouped Query Attention)
+# source: https://github.com/fkodom/grouped-query-attention-pytorch/blob/main/grouped_query_attention_pytorch/attention.py
+# let hidden_dim = num_head x embed_dim, kv_dim = kv_heads x embed_dim
+# then group_num = hidden_dim // kv_dim
+# where kv_heads < num_head for query
+# W_q: multi-head, shape (batch_size x seq_len x hidden_dim)
+# W_kv: grouped-head, shape (batch_size x seq_len x (2 * kv_dim))
+def grouped_query_attention(X_batch, W_q, W_kv, W_o, mask_batch):
+	# after Q, K, V is calculated
+	# Q shape (batch_size x num_head x seq_len x embed_dim)
+	Q = Q.reshape(batch_size, group_num, kv_heads, seq_len, embed_dim)
+	# then do the same for others
+	# K, V shape (batch_size x kv_heads x seq_len x embed_dim)
 
 # attention of llama, see https://zhuanlan.zhihu.com/p/643829565
 # implement KV cache, decoder mask, fp16 and fp32 softmax
@@ -81,6 +117,9 @@ def llama_attention(past_kvs, attention_mask, **kwargs):
 		dim=-1, dtype=torch.float32).to(Q.dtype)
 	pass
 
+def beam_search():
+	pass
+
 # xi: scalar
 # softmax scales all xi so that they sum to 1
 def softmax_i(xi, [x1, ..., xi-1, xi+1, ...]):
@@ -95,7 +134,7 @@ def softmax(X, softmax_dim=-1):
 	return X_exp / np.sum(X_exp, axis=softmax_dim, keepdims=True)
 
 # x: scalar
-# == logsitic (activation) function
+# == logistic (activation) function
 # output: range (0, 1)
 # derivative: sigmoid(x) * (1 - sigmoid(x)) = sigmoid(x) * sigmoid(- x)
 def sigmoid(x):
